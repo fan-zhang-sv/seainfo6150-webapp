ee68b700db564e83e16548abffc3e5d8111ec108
Fan Zhang <felicis_zhang@outlook.com>
2020-10-11 21:44:25 -0700

    updated App.js and DynamicArticle.jsx

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -5,7 +5,13 @@ const DynamicArticle = (props) => {
   return (
     <article>
       {/* this is an example of how you insert props into a react component */}
-      <div>{props.article.title}</div>
+      <h1>{props.article.title}</h1>
+      <address>
+          by {props.article.author} (
+          <a href={"mailto:"+props.article.authorEmail}>{props.article.authorEmail}</a>)
+          <br />
+        </address>
+        <time dateTime={props.article.timeStamp}>{props.article.displayDate} </time>
       <HTMLText text={props.article.text} />
     </article>
   );
